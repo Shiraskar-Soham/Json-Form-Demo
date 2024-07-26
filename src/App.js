8e7ef21ce97316249ad91218b72c0efcef82390e
Soham Shiraskar <soham.shiraskar@ofbusiness.in>
2024-07-26 07:38:41 +0530

    jsonforms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,155 @@
-import logo from './logo.svg';
 import './App.css';
+// import { person } from '@jsonforms/examples';
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+import React, { useState } from 'react';
+import { JsonForms } from '@jsonforms/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // const schema = person.schema;
+  // const uischema = person.uischema;
+  // const initialData = person.data;
+  const initialData = {
+    "name": "Soham Shiraskar",
+    "vegetarian": true,
+    "birthDate": "20021-12-11",
+    "personalData": {
+      "age": 21
+    },
+    "postalCode": "248006"
+  };
+
+  
+  const schema = {
+    "type": "object",
+    "properties": {
+      "name": {
+        "type": "string",
+        "minLength": 3,
+        "description": "Please enter your name"
+      },
+      "vegetarian": {
+        "type": "boolean"
+      },
+      "birthDate": {
+        "type": "string",
+        "format": "date"
+      },
+      "nationality": {
+        "type": "string",
+        "enum": [
+          "DE",
+          "IT",
+          "JP",
+          "US",
+          "RU",
+          "Other"
+        ]
+      },
+      "personalData": {
+        "type": "object",
+        "properties": {
+          "age": {
+            "type": "integer",
+            "description": "Please enter your age."
+          },
+          "height": {
+            "type": "number"
+          },
+          "drivingSkill": {
+            "type": "number",
+            "maximum": 10,
+            "minimum": 1,
+            "default": 7
+          }
+        },
+        "required": [
+          "age",
+          "height"
+        ]
+      },
+      "occupation": {
+        "type": "string"
+      },
+      "postalCode": {
+        "type": "string",
+        "maxLength": 5
+      }
+    },
+    "required": [
+      "occupation",
+      "nationality"
+    ]
+  };
+  
+  const uischema = {
+    "type": "VerticalLayout",
+    "elements": [
+      {
+        "type": "HorizontalLayout",
+        "elements": [
+          {
+            "type": "Control",
+            "scope": "#/properties/name"
+          },
+          {
+            "type": "Control",
+            "scope": "#/properties/personalData/properties/age"
+          },
+          {
+            "type": "Control",
+            "scope": "#/properties/birthDate"
+          }
+        ]
+      },
+      {
+        "type": "Label",
+        "text": "Additional Information"
+      },
+      {
+        "type": "HorizontalLayout",
+        "elements": [
+          {
+            "type": "Control",
+            "scope": "#/properties/personalData/properties/height"
+          },
+          {
+            "type": "Control",
+            "scope": "#/properties/nationality"
+          },
+          {
+            "type": "Control",
+            "scope": "#/properties/occupation",
+            "suggestion": [
+              "Accountant",
+              "Engineer",
+              "Freelancer",
+              "Journalism",
+              "Physician",
+              "Student",
+              "Teacher",
+              "Other"
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  const [data, setData] = useState(initialData);
+  return (<div className='Parent'>
+    <div className='App'>
+      <JsonForms
+        schema={schema}
+        uischema={uischema}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+        onChange={({ data, errors }) => setData(data)}
+      />
+    </div></div>
   );
 }
 
