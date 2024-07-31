3c6b0deb62a00305367c9d2b63c295c8e8195b8a
Soham Shiraskar <soham.shiraskar@ofbusiness.in>
2024-07-31 21:48:51 +0530

    Fix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './jsonforms/App';
 //import App from './jsonschema-form/App'
-// import App from './accessRequest/App';
-import App2 from './accessRequest/App2'
+import App from './accessRequest/App';
+// import App2 from './accessRequest/App2'
 // import App from './leave/App'
 
 
@@ -20,7 +20,7 @@ import App2 from './accessRequest/App2'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App2 />
-    {/* <App /> */}
+    {/* <App2 /> */}
+    <App />
   </React.StrictMode>
 );  
